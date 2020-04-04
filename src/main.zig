d7d1f86a597bfef1299c43bc2b23b4ce544a18f5
Jason Merrill <jwmerrill@gmail.com>
2020-04-04 10:20:25 -0700

    Clearer naming in main

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
-const chunk = @import("./chunk.zig");
+const Chunk = @import("./chunk.zig").Chunk;
+const OpCode = @import("./chunk.zig").OpCode;
 
 pub fn main() !void {
-  var c = chunk.Chunk.init(std.debug.global_allocator);
-  try c.write(@enumToInt(chunk.OpCode.OP_RETURN));
-  const constant = try c.addConstant(1.2);
-  try c.write(@enumToInt(chunk.OpCode.OP_CONSTANT));
-  try c.write(constant);
-  c.disassemble("test chunk");
-  c.deinit();
+  var chunk = Chunk.init(std.debug.global_allocator);
+  try chunk.write(@enumToInt(OpCode.OP_RETURN));
+  const constant = try chunk.addConstant(1.2);
+  try chunk.write(@enumToInt(OpCode.OP_CONSTANT));
+  try chunk.write(constant);
+  chunk.disassemble("test chunk");
+  chunk.deinit();
 }
